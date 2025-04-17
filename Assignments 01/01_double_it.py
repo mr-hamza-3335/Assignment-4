0cd9760d844964d7374bafc3c121e1a2a6a1d3f7
AMEER HAMZA 3335 <ameerhamza94572@gmail.com>
2025-04-17 10:41:44 -0700

    Create 01_double_it.py

---
+++ Assignments 01/01_double_it.py
@@ -0,0 +1,7 @@
+# double_it.py
+# Author: Ameer Hamza - Age: 21 - GIAIC Student
+
+print("Welcome to the Double It App!")
+num = int(input("Enter a number: "))
+double = num * 2
+print(f"The double of {num} is {double}")
