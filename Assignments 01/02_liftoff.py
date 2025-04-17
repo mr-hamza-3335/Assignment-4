ac13c227dea57c1749fd3c36237615426a012d19
AMEER HAMZA 3335 <ameerhamza94572@gmail.com>
2025-04-17 10:42:17 -0700

    Create 02_liftoff.py

---
+++ Assignments 01/02_liftoff.py
@@ -0,0 +1,11 @@
+# liftoff.py
+# Author: Ameer Hamza - Age: 21 - GIAIC Student
+
+import time
+
+print("🚀 Countdown begins...")
+for i in range(10, 0, -1):
+    print(i)
+    time.sleep(1)
+
+print("🔥 Liftoff! 🚀")
