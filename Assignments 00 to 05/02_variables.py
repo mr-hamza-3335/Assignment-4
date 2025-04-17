55e60a67b8eae2d9828bafafc11064985cfc21fe
AMEER HAMZA 3335 <ameerhamza94572@gmail.com>
2025-04-17 09:47:41 -0700

    Create 02_variables.py

---
+++ Assignments 00 to 05/02_variables.py
@@ -0,0 +1,9 @@
+# 📝 Exercise: Variables and Data Types
+
+# 1. Store your name, age, and institute in variables
+name = "Ameer Hamza"
+age = 21
+institute = "GIAIC"
+
+# 2. Print a sentence using those variables
+print(f"My name is {name}, I am {age} years old, and I study at {institute}.")
