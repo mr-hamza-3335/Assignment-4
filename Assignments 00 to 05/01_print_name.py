282d334f34d7cca76ee140acd245247a7956f6d7
AMEER HAMZA 3335 <ameerhamza94572@gmail.com>
2025-04-17 09:47:06 -0700

    Create 01_print_name.py

---
+++ Assignments 00 to 05/01_print_name.py
@@ -0,0 +1,10 @@
+# 📝 Exercise: Print Name and Age
+
+# 1. Print your name
+print("My name is Ameer Hamza")
+
+# 2. Print your age
+print("I am 21 years old")
+
+# 3. Print a sentence using both
+print("Hello, my name is Ameer Hamza and I am 21 years old.")
