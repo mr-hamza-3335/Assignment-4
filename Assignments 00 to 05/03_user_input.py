c069b1ac795e2c1d34c84ca0cb5e3f3a3750dc18
AMEER HAMZA 3335 <ameerhamza94572@gmail.com>
2025-04-17 09:48:12 -0700

    Create 03_user_input.py

---
+++ Assignments 00 to 05/03_user_input.py
@@ -0,0 +1,5 @@
+# 📝 Exercise: User Input
+
+# 1. Ask user for their favorite programming language
+language = input("What is your favorite programming language? ")
+print(f"That's great! {language} is a powerful language.")
