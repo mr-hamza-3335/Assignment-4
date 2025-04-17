2d84f318ec45bbc89105bf76405b72a14c357b58
AMEER HAMZA 3335 <ameerhamza94572@gmail.com>
2025-04-17 12:53:38 -0700

    Create Intro_to_Python.py

---
+++ Assignments 01/02_intermediate/Intro_to_Python.py
@@ -0,0 +1,17 @@
+# intro_to_python.py
+# Author: Ameer Hamza - Age: 21 - GIAIC Student
+
+print("Welcome to the Intro to Python Project!")
+
+# Taking input from user
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+# Simple Output
+print(f"Hello {name}! You are {age} years old.")
+
+# Basic Math Operations
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+sum = num1 + num2
+print(f"The sum of {num1} and {num2} is {sum}.")
