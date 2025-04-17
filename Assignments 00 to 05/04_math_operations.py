e5f9ad65b1067c2fd0500178881c21ffa98671f5
AMEER HAMZA 3335 <ameerhamza94572@gmail.com>
2025-04-17 09:48:47 -0700

    Create 04_math_operations.py

---
+++ Assignments 00 to 05/04_math_operations.py
@@ -0,0 +1,16 @@
+# 📝 Exercise: Math Operations
+
+# 1. Add two numbers
+a = 15
+b = 7
+print("Sum:", a + b)
+
+# 2. Multiply two numbers
+print("Product:", a * b)
+
+# 3. Find average of three marks
+mark1 = 80
+mark2 = 75
+mark3 = 90
+average = (mark1 + mark2 + mark3) / 3
+print("Average Marks:", average)
